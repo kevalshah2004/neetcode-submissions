e28972ce89074e5dc18a1d67ce35a07b8c61d728
Keval Mukeshbhai Shah <157234146+kevalshah2004@users.noreply.github.com>
2026-07-18 17:20:11 +0530

    Add: network-delay-time - submission-3

---
+++ Data Structures & Algorithms/network-delay-time/submission-3.java
@@ -0,0 +1,52 @@
+class Pair{
+    int node;
+    int dist;
+
+    Pair(int node, int dist){
+        this.node = node;
+        this.dist = dist;
+    }
+}
+
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int dist[] = new int[n+1];
+
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+
+        for(int i = 0; i<=n; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] time : times){
+            adj.get(time[0]).add(new Pair(time[1], time[2]));
+        }
+
+        PriorityQueue<Pair> q = new PriorityQueue<>((a,b) -> Integer.compare(a.dist, b.dist));
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        q.add(new Pair(k,0));
+        dist[k] = 0;
+
+        while(!q.isEmpty()){
+            Pair pair = q.poll();
+            int curr = pair.node;
+            if(pair.dist > dist[curr]) continue;
+            for(Pair p : adj.get(curr)){
+                int neighbor =  p.node;
+                int currDist = p.dist;
+                int newDist = currDist + dist[curr];
+                if(newDist < dist[neighbor]){
+                    q.add(new Pair(neighbor, newDist));
+                    dist[neighbor] = newDist;
+                }
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for(int i = 1; i<=n; i++){
+            if(dist[i] == Integer.MAX_VALUE) return -1;
+            if(dist[i] > max) max = dist[i];
+        }
+        return max;
+    }
+}
